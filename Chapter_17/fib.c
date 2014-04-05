1e3cbe84a6ad08bd6d0cff39dea4b447a7cf7547
Dave <snpukwana@gmail.com>
2014-04-05 15:24:53 -0400

    Chapter 17 added

---
+++ Chapter_17/fib.c
@@ -0,0 +1,6 @@
+int fib(int n) {
+    if (n < 2) {
+        return n;
+    }
+    return fib(n - 2) + fib(n - 1);
+}
---
+++ Chapter_17/fib.scm
@@ -0,0 +1,4 @@
+(define (fib n)
+    (if (< n 2) 
+        n
+        (+ (fib (- n 1)) (fib (- n 2)))))
